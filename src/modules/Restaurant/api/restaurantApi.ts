225c5c0e0bcfe8cd52199e343a6b0b6e9fe35023
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-10-15 12:42:15 +0700

    feat: init api and dto

---
+++ src/modules/Restaurant/api/restaurantApi.ts
@@ -0,0 +1,28 @@
+import { handleError } from '@/common/utils/handleError';
+
+import { AveragePrice, Restaurant } from './dto';
+
+export class RestaurantApi {
+  @handleError()
+  async Get(): Promise<Restaurant> {
+    return {
+      name: 'Restaurant',
+      description: 'Restaurant description',
+      phoneNumber: '0123456789',
+      locationLat: 10.123456,
+      locationLong: 10.123456,
+      averagePrice: AveragePrice.HundredToTwoHundred,
+      imageUrl: 'https://picsum.photos/200/300',
+      restaurantType: [
+        {
+          id: 1,
+          name: 'Restaurant type 1',
+        },
+        {
+          id: 2,
+          name: 'Restaurant type 2',
+        },
+      ],
+    };
+  }
+}
